938b2538fe8013afec459a369fc3b00b3fb42dfe
mh-shihan <mahmodul.no.1@gmail.com>
2025-08-29 23:31:12 +0600

    Add handleAllTypesOfError utility function for handling all types of error

---
+++ src/app/errors/handleAllTypesOfError.ts
@@ -0,0 +1,86 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { ZodError } from 'zod/v4';
+import AppError from './AppError';
+import handleCastError from './handleCastError';
+import handleDuplicateError from './handleDuplicateError';
+import handleValidationError from './handleValidationError';
+import handleZodError from './handleZodError';
+import { TErrorSources, TGenericErrorResponse } from '../interfaces/error';
+import getDuplicateKeyMessage from './getDuplicateKeyMessage';
+
+const handleAllTypesOfError = (err: any): TGenericErrorResponse => {
+  let statusCode = 500;
+  let message = 'Something went wrong!';
+
+  let errorSources: TErrorSources = [
+    {
+      path: '',
+      message: 'Something went wrong!',
+    },
+  ];
+
+  // Error Handling
+  if (err instanceof ZodError) {
+    const simplifiedError = handleZodError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+    // console.log('---------------Zod Error ---------------------');
+    /* ------------------------------------------*/
+  } else if (err?.name === 'ValidationError') {
+    const simplifiedError = handleValidationError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+    // console.log('--------------- Validate Error ---------------------');
+    /* ------------------------------------------*/
+  } else if (err?.name === 'CastError') {
+    const simplifiedError = handleCastError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+    // console.log('--------------- CastError Error ---------------------');
+    /* ------------------------------------------*/
+  } else if (err?.code === 11000) {
+    const simplifiedError = handleDuplicateError(err);
+    statusCode = simplifiedError?.statusCode;
+    message = simplifiedError?.message;
+    errorSources = simplifiedError?.errorSources;
+    // console.log('---------------Duplicate Error 11000---------------------');
+    /* ------------------------------------------*/
+  } else if (err instanceof AppError) {
+    statusCode = err?.statusCode;
+    message = err?.message;
+    errorSources = [
+      {
+        path: '',
+        message: err?.message,
+      },
+    ];
+    // console.log('--------------- AppError ---------------------');
+  } else if (err instanceof Error) {
+    message = err?.message;
+
+    if (typeof err?.message == 'string') {
+      message = getDuplicateKeyMessage(err?.message);
+    }
+
+    if (message === '') {
+      message = err?.message;
+    }
+    errorSources = [
+      {
+        path: '',
+        message: message,
+      },
+    ];
+  }
+
+  return {
+    statusCode,
+    message,
+    errorSources,
+  };
+};
+
+export default handleAllTypesOfError;
