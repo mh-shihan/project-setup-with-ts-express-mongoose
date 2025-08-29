ffd19ccce18f39973f552af8f4367be535fbff88
mh-shihan <mahmodul.no.1@gmail.com>
2025-08-29 23:29:48 +0600

    Add handleZodError utility function for handling zod error

---
+++ src/app/errors/handleZodError.ts
@@ -0,0 +1,27 @@
+import { ZodError } from 'zod/v4';
+import {
+  TGenericErrorResponse,
+  TZodValidationError,
+} from '../interfaces/error';
+
+const handleZodError = (err: ZodError): TGenericErrorResponse => {
+  const parsedErrors = JSON.parse(err.message);
+
+  const errors: TZodValidationError[] = parsedErrors;
+
+  const errorSources = errors.map((error) => {
+    return {
+      path: error?.path[error?.path?.length - 1],
+      message: error.message,
+    };
+  });
+  const statusCode = 400;
+
+  return {
+    statusCode,
+    message: 'Validation Error',
+    errorSources,
+  };
+};
+
+export default handleZodError;
