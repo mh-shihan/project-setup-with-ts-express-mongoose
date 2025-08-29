59fe635980a4e56033044e8c21c0fadcda178539
mh-shihan <mahmodul.no.1@gmail.com>
2025-08-29 22:59:30 +0600

    Set up routing by adding router middleware and creating routes index file

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
+import router from './app/routes';
 
 const app: Application = express();
 
@@ -8,6 +9,8 @@ const app: Application = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({ origin: ['http://localhost:5173'] }));
+
+app.use('/api/v1', router);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
