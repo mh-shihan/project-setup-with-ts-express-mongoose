0d4e9fc2fd4677299c03de5db6d032d6e779f12e
mh-shihan <mahmodul.no.1@gmail.com>
2025-08-29 23:49:42 +0600

    Every thing is ok: Ready to go

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import router from './app/routes';
+import notFound from './app/middlewares/notFound';
+import globalErrorHandler from './app/middlewares/globalErrorHandler';
 
 const app: Application = express();
 
@@ -15,5 +17,11 @@ app.use('/api/v1', router);
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
 });
+
+// Global Error Handler
+app.use(globalErrorHandler);
+
+// Not Found Route
+app.use(notFound);
 
 export default app;
