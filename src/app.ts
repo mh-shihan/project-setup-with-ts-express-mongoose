b03052e94f6c46753ba784a5fe5d16b87e19baab
mh-shihan <mahmodul.no.1@gmail.com>
2025-08-29 21:18:49 +0600

    Add bcrypt and cookie-parser dependencies; implement catchAsync utility

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,13 @@
-import express, { Request, Response } from 'express';
+import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
-const app = express();
+const app: Application = express();
 
 // Parser
 app.use(express.json());
-app.use(cors());
+app.use(cookieParser());
+app.use(cors({ origin: ['http://localhost:5173'] }));
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World!');
